88476e7fffbc7727414ccf6b44e2cfe1361f16b4
Shoaiyb Sysa <75518689+shoaiyb@users.noreply.github.com>
2023-05-12 19:27:28 +0100

    Add files via upload

--- plugin.php
+++ plugin.php
@@ -3,29 +3,28 @@
  *
  * RSS Feed
  *
- * @package BoidCMS
- * @subpackage Feed
- * @author Shoaiyb Sysa
+ * @package Plugin_Feed
+ * @author Shuaib Yusuf Shuaib
  * @version 1.0.0
  */
 
 global $App;
-$App->set_action( 'install', 'feed_init' );
-$App->set_action( 'uninstall', 'feed_shut' );
-$App->set_action( 'slug_taken', 'feed_slug' );
+$App->set_action( 'install', 'feed_install' );
+$App->set_action( 'uninstall', 'feed_uninstall' );
+$App->set_action( array( 'create_success', 'update_success', 'delete_success', 'generate_feed' ), 'generate_feed' );
 $App->set_action( 'site_head', 'feed_head' );
-$App->set_action( 'render', 'feed_render' );
 $App->set_action( 'admin', 'feed_admin' );
 
 /**
- * Initiate Feed, first time install
+ * Initialize Feed, first time install
  * @param string $plugin
  * @return void
  */
-function feed_init( string $plugin ): void {
+function feed_install( string $plugin ): void {
   global $App;
   if ( 'feed' === $plugin ) {
     $App->set( 5, 'feed' );
+    generate_feed();
   }
 }
 
@@ -34,7 +33,7 @@ function feed_init( string $plugin ): void {
  * @param string $plugin
  * @return void
  */
-function feed_shut( string $plugin ): void {
+function feed_uninstall( string $plugin ): void {
   global $App;
   if ( 'feed' === $plugin ) {
     $App->unset( 'feed' );
@@ -64,27 +63,20 @@ function feed_get( string $index, bool $esc = true ): string {
 }
 
 /**
- * Feed taken slug
- * @return string
- */
-function feed_slug(): string {
-  return ',feed/,';
-}
-
-/**
  * HTML head link
  * @return string
  */
 function feed_head(): string {
-  $html = '<link rel="alternate" type="application/rss+xml" title="%s Feed" href="%sfeed/">';
+  $html = '<link rel="alternate" type="application/rss+xml" title="%s Feed" href="%sfeed.xml">';
   return sprintf( $html, feed_get( 'title' ), feed_get( 'url' ) );
 }
 
 /**
  * Feed posts
+ * @param ?int $max
  * @return array
  */
-function feed_posts(): array {
+function feed_posts( ?int $max = null ): array {
   global $App;
   $posts = array();
   $pages = $App->data()[ 'pages' ];
@@ -93,23 +85,22 @@ function feed_posts(): array {
       $posts[ $slug ] = $p;
     }
   }
-  $max = $App->get( 'feed' );
+  $max ??= $App->get( 'feed' );
   return array_slice( $posts, 0, $max, true );
 }
 
 /**
  * Feed XML
- * @return void
+ * @return DOMDocument
  */
-function feed_xml(): void {
+function feed_xml(): DOMDocument {
   $posts = feed_posts();
-  header( 'Content-Type: text/xml' );
   $xml = '<?xml version="1.0" encoding="UTF-8"?>';
   $xml .= '<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">';
   $xml .= '<channel>';
   $xml .= '<title>' . feed_get( 'title' ) . '</title>';
   $xml .= '<link>' . feed_get( 'url' ) . '</link>';
-  $xml .= '<atom:link href="' . feed_get( 'url' ) . 'feed/" rel="self" type="application/rss+xml"/>';
+  $xml .= '<atom:link href="' . feed_get( 'url' ) . 'feed.xml" rel="self" type="application/rss+xml"/>';
   $xml .= '<description>' . strip_tags( feed_get( 'descr', false ) ) . '</description>';
   $xml .= '<language>' . feed_get( 'lang' ) . '</language>';
   $xml .= '<lastBuildDate>' . date( DATE_RSS ) . '</lastBuildDate>';
@@ -125,25 +116,21 @@ function feed_xml(): void {
   }
   $xml .= '</channel>';
   $xml .= '</rss>';
-  $dom = new DOMDocument();
-  $dom->formatOutput = true;
-  $dom->loadXML( $xml );
-  $xml = $dom->saveXML();
-  $xml = trim( $xml );
-  exit( $xml );
+  $doc = new DOMDocument();
+  $doc->formatOutput = true;
+  $doc->loadXML( $xml );
+  return $doc;
 }
 
 /**
- * XML Feed page render
- * @return void
+ * Feed generator
+ * @return bool
  */
-function feed_render(): void {
+function generate_feed(): bool {
   global $App;
-  switch ( $App->page ) {
-    case 'feed/':
-      feed_xml();
-      break;
-  }
+  $doc = feed_xml();
+  $file = $App->root( 'feed.xml' );
+  return ( bool ) $doc->save( $file );
 }
 
 /**
@@ -159,6 +146,7 @@ function feed_admin(): void {
       <form action="' . $App->admin_url( '?page=feed', true ) . '" method="post">
         <label for="feed" class="ss-label">Number of items to display <span class="ss-red">*</span></label>
         <input type="number" id="feed" name="feed" min="1" value="' . $App->get( 'feed' ) . '" class="ss-input ss-mobile ss-w-6 ss-mx-auto" required>
+        <p class="ss-small ss-gray ss-mb-5">This is the maximum number of posts to show in the feed.</p>
         <input type="hidden" name="token" value="' . $App->token() . '">
         <input type="submit" name="save" value="Save" class="ss-btn ss-mobile ss-w-5">
       </form>';
